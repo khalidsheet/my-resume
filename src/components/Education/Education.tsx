6c55cab4c07fe3291a589d641a8cfbef7b0b5008
Khalid Sheet <khalid.m.sheet@gmail.com>
2023-10-24 03:26:35 +0300

    added education section

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,10 +1,24 @@
+import { ExpTile } from "../Work/components/ExpTile/ExpTile";
+
 export const Education = () => {
   return (
-    <div className="mt-12">
+    <div className="mt-12 max-w-6xl mx-auto">
       <h1 className="text-2xl uppercase text-center mb-4 font-bold">
         Education
       </h1>
-      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4"></div>
+      <div className="p-4">
+        <ExpTile
+          data={{
+            company: "Harbour.Space",
+            description: "sss",
+            isPresent: false,
+            jobTitle: "Front End Development",
+            location: "Barcelona, Spain",
+            startEndDate: "2022 Oct - 2025 Sep",
+          }}
+          noDescription
+        />
+      </div>
     </div>
   );
 };
--- src/components/Work/components/ExpTile/ExpTile.tsx
+++ src/components/Work/components/ExpTile/ExpTile.tsx
@@ -1,7 +1,13 @@
 import { CalendarDaysIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import { WorkTileData } from "../../../../data/work";
 
-export const ExpTile = ({ data }: { data: WorkTileData }) => {
+export const ExpTile = ({
+  data,
+  noDescription = false,
+}: {
+  data: WorkTileData;
+  noDescription?: boolean;
+}) => {
   return (
     <div
       className={`border bg-gray-900 border-gray-800 mb-4 p-6 rounded-lg ${
@@ -21,13 +27,20 @@ export const ExpTile = ({ data }: { data: WorkTileData }) => {
           {data.company}
         </span>
       </div>
-      <div className="working-dates text-gray-500 text-sm flex items-center gap-x-2">
+      <div
+        className={`working-dates text-gray-500 text-sm flex items-center gap-x-2 ${
+          noDescription ? "mt-2 py-0.5 md:float-left mr-2" : ""
+        }`}
+      >
         <CalendarDaysIcon className="w-4 h-4 text-gray-500" />
         {data.startEndDate} {data.isPresent ? " - Present" : null}
       </div>
-      <div className="description mt-2 text-gray-400 min-h-[180px]">
-        {data.description}
-      </div>
+      {!noDescription && (
+        <div className="description mt-2 text-gray-400 min-h-[180px]">
+          {data.description}
+        </div>
+      )}
+
       <div className="location mt-2 text-gray-600 flex gap-x-2 items-center">
         <MapPinIcon className="w-4 h-4 text-gray-500" />
         {data.location}
